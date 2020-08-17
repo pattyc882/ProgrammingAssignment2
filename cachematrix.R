1118895808fc033acbae0979f28742a5727e2e0f
pattyc882 <68076316+pattyc882@users.noreply.github.com>
2020-08-16 21:30:03 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+#create a matrix and cache its inverse 
+
 
 ## Write a short comment describing this function
 #Creates makeCacheMatrix function, initializes variables, creates set, get, 
